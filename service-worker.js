5cfa23286b79a7705f8063703930a75bd8fdf062
irawandery96 <irawandery96@gmail.com>
2026-09-23 08:33:22 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+// Service Worker sederhana untuk PWA
+const CACHE_NAME = 'keuangan-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
